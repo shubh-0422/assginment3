b09457db334d4236a0eb43ee7260a72904f758d9
Shubham Kumawat <Shubham Kumawat@DESKTOP-7BH01OF>
2023-01-12 13:15:33 +0530

    Add project files.

---
+++ assginment3/Counter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace assginment3
+{
+    class Counter
+    {
+
+        private static int Count;
+        private static Counter Instance;
+
+        public static Counter GetInstance() {
+            Instance = new Counter();
+            return Instance;
+        }        
+        public void Increment() {
+            ++Count;
+        }
+        public void Decrement() {
+            if (Count >= 0) {
+                --Count;
+            }
+        }
+        public void printCountValue() {
+            Console.WriteLine("Counter current Value: "+Count);
+        }
+
+    }
+}
---
+++ assginment3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace assginment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counterOblect = Counter.GetInstance();
+            counterOblect.printCountValue();
+
+            counterOblect.Increment();
+            counterOblect.Increment(); 
+            counterOblect.printCountValue();
+
+            counterOblect.Decrement(); 
+            counterOblect.printCountValue();
+        }
+    }
+}
